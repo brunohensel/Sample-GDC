e7b9e3e8399405c462c31eeb42cebf84c49d6a0e
brunohensel <bruno.hensel@gmail.com>
2020-11-18 19:54:25 +0100

    implemented jobService/Scheduler

--- app/src/main/java/com/example/samplegdc/feature/taskAdd/TaskAddActivity.kt
+++ app/src/main/java/com/example/samplegdc/feature/taskAdd/TaskAddActivity.kt
@@ -2,9 +2,13 @@ package com.example.samplegdc.feature.taskAdd
 
 import android.app.DatePickerDialog
 import android.app.TimePickerDialog
+import android.app.job.JobInfo
+import android.app.job.JobScheduler
+import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.view.MenuItem
 import android.widget.DatePicker
 import android.widget.TimePicker
@@ -19,10 +23,12 @@ import com.example.samplegdc.data.entity.TaskDto
 import com.example.samplegdc.domain.TaskRepository
 import com.example.samplegdc.feature.DatePickerFragment
 import com.example.samplegdc.feature.TimePickerFragment
+import com.example.samplegdc.job.NotificationJobService
 import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_task_new.*
 import org.threeten.bp.OffsetDateTime
+import org.threeten.bp.temporal.ChronoUnit
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -36,6 +42,8 @@ class TaskAddActivity : AppCompatActivity(), DatePickerDialog.OnDateSetListener,
     lateinit var repository: TaskRepository
 
     private var taskDate: TaskDateDto = TaskDateDto()
+
+    private lateinit var taskDto: TaskDto
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -66,12 +74,34 @@ class TaskAddActivity : AppCompatActivity(), DatePickerDialog.OnDateSetListener,
                 } else {
                     Toast.makeText(this, R.string.date_not_set, Toast.LENGTH_LONG).show()
                 }
-                viewModel.insert(TaskDto(name = name, createdAt = offsetDateTime))
+                taskDto = TaskDto(name = name, createdAt = offsetDateTime)
+                viewModel.insert(taskDto)
+                createAJobScheduler(taskDto)
                 finish()
             } else {
                 Snackbar.make(edtTaskNewName, R.string.name_required, Snackbar.LENGTH_LONG).show()
             }
         }
+    }
+
+    /**Create a JobScheduler
+     * [serviceName] is used to associate the JobService with the JobInfo object.
+     * [timeTilFuture] is the time between the actual date and the date set for the task
+     */
+    private fun createAJobScheduler(taskDto: TaskDto) {
+        val scheduler = getSystemService(Context.JOB_SCHEDULER_SERVICE) as JobScheduler
+        val serviceName = ComponentName(packageName, NotificationJobService::class.java.name)
+
+        val timeTilFuture = ChronoUnit.MILLIS.between(OffsetDateTime.now(), taskDto.createdAt)
+
+        val jobBuilder = JobInfo.Builder(0, serviceName)
+            .setMinimumLatency(timeTilFuture)
+
+        val extras = PersistableBundle()
+        extras.putString(SCHEDULE_EXTRA_TASK_NAME, taskDto.name)
+
+        val jobInfo = jobBuilder.setExtras(extras).build()
+        scheduler.schedule(jobInfo)
     }
 
     private fun showTimePickerDialog() {
@@ -92,14 +122,6 @@ class TaskAddActivity : AppCompatActivity(), DatePickerDialog.OnDateSetListener,
         return false
     }
 
-    companion object {
-        /**Start [TaskAddActivity]
-         * @param context previous activity*/
-        fun start(context: Context): Intent {
-            return Intent(context, TaskAddActivity::class.java)
-        }
-    }
-
     private fun setTaskDateTime(taskDateDto: TaskDateDto): OffsetDateTime {
         return OffsetDateTime.of(
             taskDateDto.year,
@@ -114,7 +136,7 @@ class TaskAddActivity : AppCompatActivity(), DatePickerDialog.OnDateSetListener,
     }
 
     override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
-        taskDate = taskDate.copy(day = dayOfMonth, month = month, year = year)
+        taskDate = taskDate.copy(day = dayOfMonth, month = month + 1, year = year)
         val date = "$dayOfMonth/${this.taskDate.month}/$year"
         txtTaskNewDate.text = date
     }
@@ -123,5 +145,17 @@ class TaskAddActivity : AppCompatActivity(), DatePickerDialog.OnDateSetListener,
         taskDate = taskDate.copy(minute = minute, hour = hourOfDay)
         val time = "$hourOfDay:$minute"
         txtTaskNewTime.text = time
+    }
+
+
+    companion object {
+
+        const val SCHEDULE_EXTRA_TASK_NAME = "SCHEDULE_EXTRA_TASK_NAME"
+
+        /**Start [TaskAddActivity]
+         * @param context previous activity*/
+        fun start(context: Context): Intent {
+            return Intent(context, TaskAddActivity::class.java)
+        }
     }
 }
---
+++ app/src/main/java/com/example/samplegdc/job/NotificationJobService.kt
@@ -0,0 +1,43 @@
+package com.example.samplegdc.job
+
+import android.app.job.JobParameters
+import android.app.job.JobService
+import android.util.Log
+import com.example.samplegdc.feature.taskAdd.TaskAddActivity
+
+/**
+ * The Service that JobScheduler runs once the conditions are met.
+ * In this case it posts a notification.
+ */
+class NotificationJobService : JobService() {
+    /**
+     * Called by the system once it determines it is time to run the job.
+     *
+     * @param jobParameters Contains the information about the job.
+     * @return Boolean indicating whether or not the job was offloaded to a
+     * separate thread.
+     * In this case, it is false since the notification can be posted on
+     * the main thread.
+     */
+    override fun onStartJob(jobParameters: JobParameters): Boolean {
+        val taskName = jobParameters.extras.getString(TaskAddActivity.SCHEDULE_EXTRA_TASK_NAME)
+        Log.i(
+            NotificationJobService::class.java.name,
+            taskName ?: throw  IllegalArgumentException("no name")
+        )
+        return false
+    }
+
+    /**
+     * Called by the system when the job is running but the conditions are no
+     * longer met.
+     * In this example it is never called since the job is not offloaded to a
+     * different thread.
+     *
+     * @param jobParameters Contains the information about the job.
+     * @return Boolean indicating whether the job needs rescheduling.
+     */
+    override fun onStopJob(jobParameters: JobParameters): Boolean {
+        return false
+    }
+}
